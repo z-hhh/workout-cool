0a380521bc4f034bc6376fddea861a1a80f19487
Mathias <mathiasnouzy@gmail.com>
2025-06-27 22:35:16 +0200

    feat(middleware): update matcher to exclude ads.txt from middleware processing feat(ads.txt): add ads.txt file for ad verification purposes

--- middleware.ts
+++ middleware.ts
@@ -95,6 +95,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!api|static|_next|manifest.json|favicon.ico|robots.txt|sw.js|apple-touch-icon.png|android-chrome-.*\\.png|images|icons|sitemap.xml).*)",
+    "/((?!api|static|_next|manifest.json|favicon.ico|robots.txt|sw.js|apple-touch-icon.png|android-chrome-.*\\.png|images|icons|sitemap.xml|ads.txt).*)",
   ],
 };
