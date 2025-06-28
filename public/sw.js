0d6ebddd3d6e68e1253cc9763f12aee25de9f252
Mathias <mathiasnouzy@gmail.com>
2025-06-28 15:33:07 +0200

    refactor(sw.js): simplify fetch event handling by removing caching logic to streamline service worker functionality

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,5 @@
 const CACHE_NAME = "1.2.5";
 const urlsToCache = [
-  "/",
   "/manifest.json",
   "/images/favicon-32x32.png",
   "/images/favicon-16x16.png",
@@ -39,42 +38,7 @@ self.addEventListener("fetch", (event) => {
     return;
   }
 
-  event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        // Don't cache non-successful responses
-        if (!response || response.status !== 200 || response.type !== "basic") {
-          return response;
-        }
-
-        // Clone the response
-        const responseToCache = response.clone();
-
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, responseToCache);
-        });
-
-        return response;
-      })
-      .catch(() => {
-        // Network failed, try cache
-        return caches.match(event.request).then((cachedResponse) => {
-          if (cachedResponse) {
-            return cachedResponse;
-          }
-          // If no cache available, return a custom offline page or error
-          if (event.request.destination === "document") {
-            return new Response("Application offline - Please check your internet connection", {
-              status: 503,
-              statusText: "Service Unavailable",
-              headers: new Headers({
-                "Content-Type": "text/html; charset=utf-8",
-              }),
-            });
-          }
-        });
-      }),
-  );
+  event.respondWith(fetch(event.request));
 });
 
 // Activate event - clean up old caches
